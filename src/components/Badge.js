4711642e59697cffa945a0c9df251cf27d725b5e
Rafael Soriano Ramírez <rafael.soriano@arteenarte.com>
2020-09-29 06:12:59 -0500

    Primer componente

---
+++ src/components/Badge.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import "./styles/Badge.css"
+import confLogo from '../images/badge-header.svg'
+
+class Badge extends React.Component {
+
+  render () {
+    const { firstName, lastName, jobTitle, twitter } = this.props;
+
+    return (
+      <div className="Badge">
+        <div className="Badge__header">
+          <img src={confLogo} alt="Logo de la conferencia"/>
+        </div>
+
+        <div className="Badge__section-name">
+          <img 
+            className="Badge__avatar" 
+            src="https://www.gravatar.com/avatar?d=identicon" 
+            alt="Avatar"
+          />
+          <h1>{firstName} <br /> {lastName}</h1>
+        </div>
+
+        <div className="Badge__section-info">
+          <h3>{jobTitle}</h3>
+          <div>{twitter}</div>
+        </div>
+
+        <div className="Badge__footer">
+          #platziconf
+        </div>
+      </div>
+    );
+  };
+}
+
+export default Badge;
--- src/index.js
+++ src/index.js
@@ -1,34 +1,17 @@
-// JS ES
-//const element = document.createElement('h1');
- 
-//element.innerText = 'Hello, Platzi badgets';
-
-//const container = document.getElementById('app');
-
-//container.appendChild(element);
-
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
 
-//const jsx = <h1>Hello, Platzi Badges from React & ReactDom!</h1>
-//const element = React.createElement('h1', {}, 'Hola!, soy los children');
-//const element = React.createElement(
-//  'a', 
-//  {href: 'https://platzi.com'}, 
-//  'Ir a Platzi'
-//);
-//const name = 'Rafa';
-//const element = React.createElement(
-//  'h1',
-//  {},
- // `Hola soy ${name}`
-//);
-//const jsx = <h1>Hola soy {name}</h1>
-const jsx = <div>
-  <h1>Hola, soy Rafa</h1>
-  <p>Soy ingeniero Frontend</p>
-</div>
+import 'bootstrap/dist/css/bootstrap.css';
+import './global.css';
+import Badge from './components/Badge';
+
 const container = document.getElementById('app');
 
-//ReactDom.render(__qué__, __donde__);
-ReactDom.render(jsx, container);
+ReactDOM.render(
+  <Badge 
+    firstName="Rafael" 
+    lastName="Soriano" 
+    jobTitle="Frontend Engineer" 
+    twitter="@frontendbadass"
+  />,
+container);
